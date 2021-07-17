9254596708f6e0381b40fa300c127d02bac505d9
Arvind <78128658+Rv1411@users.noreply.github.com>
2021-07-17 14:07:35 -0500

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -3,10 +3,10 @@ create database portfolio_manager;
 use portfolio_manager;
 
 create table user(
-			user_id INT AUTO_INCREMENT, 
-            user_name varchar(20), 
-            password varchar(20), 
-            email_id varchar(20), 
-            contact varchar(15),
-            PRIMARY KEY (user_id)
+	user_id INT AUTO_INCREMENT, 
+        user_name varchar(20), 
+        password varchar(20),
+	email_id varchar(20), 
+        contact varchar(15),
+        PRIMARY KEY (user_id)
       );
