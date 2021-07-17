407f17f37805a3e73e1e6a7c9a0a519662431d8d
Arvind <78128658+Rv1411@users.noreply.github.com>
2021-07-17 14:07:07 -0500

    creaeting user table

--- DB.sql
+++ DB.sql
@@ -1,1 +1,12 @@
+create database portfolio_manager;
 
+use portfolio_manager;
+
+create table user(
+			user_id INT AUTO_INCREMENT, 
+            user_name varchar(20), 
+            password varchar(20), 
+            email_id varchar(20), 
+            contact varchar(15),
+            PRIMARY KEY (user_id)
+      );
