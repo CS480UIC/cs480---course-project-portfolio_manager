7e8a5bfe18d8d75ec58bd2133294a5211d125e50
Mishra <vmishr8@uic.edu>
2021-07-30 23:56:52 -0500

    added gitignore

--- PortfolioManager/src/main/java/edu/uic/cs480/model/Portfolio.java
+++ PortfolioManager/src/main/java/edu/uic/cs480/model/Portfolio.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-@Entity(name = "portofolio")
+//@Entity(name = "portfolio")
 public class Portfolio {
 
 	@ManyToOne
